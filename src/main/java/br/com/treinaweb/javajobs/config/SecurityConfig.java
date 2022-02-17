fa5c9d3515f173aee97716351797505572e7f7a2
delper <daniel@delper.com.br>
2022-02-17 19:46:05 -0300

    Versão até módulo 7.2 Controller Usuario e Spring Security Core

---
+++ src/main/java/br/com/treinaweb/javajobs/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.treinaweb.javajobs.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.treinaweb.javajobs.services.AuthenticationService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String API_JOBS_URL = "/api/v1/jobs/**";
+    private static final String API_USERS_URL = "/api/v1/users/**";
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+            .disable();
+
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, API_JOBS_URL).permitAll()
+            .antMatchers(API_USERS_URL).permitAll()
+            .anyRequest().authenticated();
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService)
+            .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
